d61c64900e9211b70d2f77e0f5c88c5546f1b921
rnobleeddy <rnobleeddy@gmail.com>
2017-04-28 16:47:14 +0100

    Update getPrimeFactors.R

--- primeFactors/R/getPrimeFactors.R
+++ primeFactors/R/getPrimeFactors.R
@@ -6,6 +6,8 @@
 #' \code{\link{isPrime}} functions. It uses \code{\link{getFactors}} to get all factors and
 #' \code{\link{isPrime}} to return only those that are prime.
 #'  
+#' @export
+#'  
 #' @param x An integer to generate the prime factors for
 #'  
 #' @examples
@@ -13,6 +15,11 @@
 #' getPrimeFactors(7)
 
 getPrimeFactors <- function(x) {
+  
+  # Good practise to check input validity - aim for the package to fail gracefully!
+  if(!is.numeric(x) || x%%1!=0 || x < 1) {
+    stop("getPrimeFactors can only be called with a positive integer")
+  }
   
   fcts <- getFactors(x)
   
