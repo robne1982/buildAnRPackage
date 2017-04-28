66cf68190251ed331f259acfaf04c0aefd74b61a
rnobleeddy <rnobleeddy@gmail.com>
2017-04-28 16:46:52 +0100

    Update getLargestPrimeFactor.R

--- primeFactors/R/getLargestPrimeFactor.R
+++ primeFactors/R/getLargestPrimeFactor.R
@@ -6,11 +6,18 @@
 #' all prime factors of \code{x} and then returns the largest
 #'  
 #' @param x An integer to generate the largest prime factor for
+#' 
+#' @export
 #'  
 #' @examples
 #' getLargestPrimeFactor(21)
 
 getLargestPrimeFactor <- function(x) {
+  
+  # Good practise to check input validity - aim for the package to fail gracefully!
+  if(!is.numeric(x) || x%%1!=0 || x < 1) {
+    stop("getLargestPrimeFactor can only be called with a positive integer")
+  }
   
   primeFcts <- getPrimeFactors(x)
   return(max(primeFcts))
