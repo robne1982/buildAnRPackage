9a1e963320b59f2f0060faae871baed6ca367c7d
rnobleeddy <rnobleeddy@gmail.com>
2017-05-02 09:22:57 +0100

    Update demo3.R

--- demo3.R
+++ demo3.R
@@ -1,7 +1,11 @@
+# ------ DEMO 3 ------
+# Exploring the package we built to solve Project Euler Q3
+
+# Clear the environment and load the functions we've written
 rm(list=ls())
 devtools::load_all()
 
-# Example of exported functions - two colons
+# Example of exported functions - two colons (or none!)
 library(dplyr)
 dplyr::
 # internal functions via three colons
@@ -10,23 +14,23 @@ dplyr:::
 
 # Lets run through the functions we we wrote for the package
 
-# ---- isPrime ------
+# ---- isPrime ----
 isPrime(1)
 isPrime(2)
 isPrime(21)
 isPrime(23)
 
-# getFactors
+# ---- getFactors ----
 getFactors(10)
 getFactors(50)
 getFactors(100)
 
-# getPrimeFactors
+# ---- getPrimeFactors ----
 getPrimeFactors(10)
 getPrimeFactors(50)
 getPrimeFactors(100)
 
-# getLargestPrimeFactor
+# ---- getLargestPrimeFactor ----
 getLargestPrimeFactor(10)
 getLargestPrimeFactor(50)
 getLargestPrimeFactor(100)
